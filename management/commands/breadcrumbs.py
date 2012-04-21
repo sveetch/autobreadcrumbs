04c3392aa80b78b8dc24c01c561fd054a6308ad5
David THENON <sveetch@gmail.com>
2012-04-21 18:14:04 +0200

    Correct a bug on the management command

--- management/commands/breadcrumbs.py
+++ management/commands/breadcrumbs.py
@@ -29,21 +29,22 @@ class Command(BaseCommand):
         if not self.excluded_paths:
             self.excluded_paths = []
 
-        # Parcours des urls depuis la racine
+        # Walking through all urls, starting from the root UrlConf
         urlconf = settings.ROOT_URLCONF
         rootresolver = RegexURLResolver(r'^/', urlconf)
         self._walk_urlresolver("/", rootresolver)
     
     def _walk_urlresolver(self, parent, urlresolver, lv=0):
-        for item in urlresolver.url_patterns:
-            current_path = parent + self._clear_regexpath(item.regex.pattern)
-            if startswith_in_list(current_path, self.excluded_paths):
-                continue
-            if not isinstance(item, RegexURLResolver):
-                print "{indent}* {path} {name}".format(indent=("  "*lv), path=colorize(current_path, fg='green'), name=colorize(item.name, fg='magenta'))
-            else:
-                print "{indent}* {path}".format(indent=("  "*lv), path=colorize(current_path, fg='blue'))
-                self._walk_urlresolver(current_path, item, lv=lv+1)
+        if hasattr(urlresolver, 'url_patterns'):
+            for item in urlresolver.url_patterns:
+                current_path = parent + self._clear_regexpath(item.regex.pattern)
+                if startswith_in_list(current_path, self.excluded_paths):
+                    continue
+                if not isinstance(item, RegexURLResolver) and getattr(item, 'name', None):
+                    print "{indent}* {path} {name}".format(indent=("  "*lv), path=colorize(current_path, fg='green'), name=colorize(item.name, fg='magenta'))
+                else:
+                    print "{indent}* {path}".format(indent=("  "*lv), path=colorize(current_path, fg='blue'))
+                    self._walk_urlresolver(current_path, item, lv=lv+1)
     
     def _clear_regexpath(self, regex):
         path = regex
