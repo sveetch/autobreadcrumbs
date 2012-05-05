3e3d5f8c65e28b14f8ed882db9f1163d69787280
David THENON <sveetch@gmail.com>
2012-05-05 21:41:03 +0200

    Correct bad link on setup.py and correct MANIFEST

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     long_description=open('README.rst').read(),
     author='David Thenon',
     author_email='sveetch@gmail.com',
-    url='https://github.com/sveetch/autobreadcrumbs',
+    url='http://pypi.python.org/pypi/autobreadcrumbs',
     license='MIT',
     packages=find_packages(),
     classifiers=[
