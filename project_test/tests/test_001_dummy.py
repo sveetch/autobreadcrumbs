6b26c5c54972632db6708628ebebd933cf56ea21
David THENON <sveetch@gmail.com>
2016-08-06 00:40:12 +0200

    Added clean structure for unittests with py.test

---
+++ project_test/tests/test_001_dummy.py
@@ -0,0 +1,9 @@
+import pytest
+
+from django.core.urlresolvers import reverse
+
+
+def test_ping_index(client):
+    """Just pinging dummy homepage"""
+    response = client.get(reverse('home'))
+    assert response.status_code == 200
